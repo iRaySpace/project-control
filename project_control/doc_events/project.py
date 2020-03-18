2e008c9dc9f6fc4d83e059632f740f0ed684aee9
Ivan Ray Altomera <irayspacii@gmail.com>
2020-03-18 21:55:59 +0800

    feat: calculation for variation and estimated total

--- project_control/doc_events/project.py
+++ project_control/doc_events/project.py
@@ -3,8 +3,19 @@ from functools import reduce
 
 def validate(project, method):
     _set_budget_total(project)
+    _set_variation_total(project)
+    _set_estimated_total(project)
 
 
 def _set_budget_total(project):
     budget_total = reduce(lambda total, budget: total + budget.amount, project.pc_budgets, 0.0)
     project.pc_budget_total = budget_total
+
+
+def _set_variation_total(project):
+    variation_total = reduce(lambda total, budget: total + budget.amount, project.pc_variations, 0.0)
+    project.pc_variation_total = variation_total
+
+
+def _set_estimated_total(project):
+    project.pc_estimated_total = sum([project.pc_budget_total, project.pc_variation_total])

91b596d14d25def95ad9b7231a9a88f324be5140
Ivan Ray Altomera <irayspacii@gmail.com>
2020-06-08 16:29:56 +0800

    feat(gl_entry): include Stock Entry set project

--- project_control/doc_events/gl_entry.py
+++ project_control/doc_events/gl_entry.py
@@ -4,5 +4,5 @@ import frappe
 # Fix for Non-PL (project not set)
 def validate(doc, method):
     voucher_type = doc.voucher_type
-    if voucher_type in ['Sales Invoice', 'Delivery Note', 'Purchase Invoice']:
+    if voucher_type in ['Sales Invoice', 'Delivery Note', 'Purchase Invoice', 'Stock Entry']:
         doc.project = frappe.db.get_value(voucher_type, doc.voucher_no, 'project')

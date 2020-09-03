8158e1cb0b193b113a8b55b891d3cac898994d87
Ivan Ray Altomera <irayspacii@gmail.com>
2020-09-03 19:26:31 +0800

    feat: ignore stock entry value

--- project_control/data.py
+++ project_control/data.py
@@ -5,18 +5,28 @@ from frappe import _
 def validate_project_costing(project, cost):
     estimated_total = frappe.get_value('Project', project, 'pc_estimated_total')
     total_purchase_cost = frappe.get_value('Project', project, 'total_purchase_cost')
-    total_consumed_material_cost = frappe.get_value('Project', project, 'total_consumed_material_cost')
 
-    total_costing = total_purchase_cost + total_consumed_material_cost
-    if total_costing + cost > estimated_total:
+    total_costing = sum([total_purchase_cost, cost])
+    if not _get_ignore_se():
+        total_consumed_material_cost = frappe.get_value('Project', project, 'total_consumed_material_cost')
+        total_costing = total_costing + total_consumed_material_cost
+
+    if total_costing > estimated_total:
         frappe.throw(_('Transaction is not allowed. Total purchase cost will exceed project estimated value.'))
 
 
 def validate_project_costing_for_warning(project, cost):
     estimated_total = frappe.get_value('Project', project, 'pc_estimated_total')
     total_purchase_cost = frappe.get_value('Project', project, 'total_purchase_cost')
-    total_consumed_material_cost = frappe.get_value('Project', project, 'total_consumed_material_cost')
 
-    total_costing = total_purchase_cost + total_consumed_material_cost
-    if total_costing + cost > estimated_total:
+    total_costing = sum([total_purchase_cost, cost])
+    if not _get_ignore_se():
+        total_consumed_material_cost = frappe.get_value('Project', project, 'total_consumed_material_cost')
+        total_costing = total_costing + total_consumed_material_cost
+
+    if total_costing > estimated_total:
         frappe.msgprint(_('Total purchase cost will exceed project estimated value.'))
+
+
+def _get_ignore_se():
+    return frappe.db.get_single_value('Project Control Settings', 'ignore_se')

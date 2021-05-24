ae861930753e4861f04b00674b4ec932a3005533
Ivan Ray Altomera <irayspacii@gmail.com>
2021-05-24 16:08:42 +0800

    format(project): indents

--- project_control/doc_events/project.py
+++ project_control/doc_events/project.py
@@ -14,46 +14,63 @@ def validate(project, method):
 # TODO: dynamic via Settings
 def after_insert(project, method):
     tasks = [
-        'Advance Payment',
-        'Supplier LPO',
-        'Supplier Payment',
-        'Custom Clearance & Payment',
-        'Receive Goods',
-        'Delivery',
-        'Installation',
-        'Payment'
+        "Advance Payment",
+        "Supplier LPO",
+        "Supplier Payment",
+        "Custom Clearance & Payment",
+        "Receive Goods",
+        "Delivery",
+        "Installation",
+        "Payment",
     ]
     for task in tasks:
-        frappe.get_doc({
-            'doctype': 'Task',
-            'subject': task,
-            'project': project.name
-        }).insert()
-    frappe.msgprint(_('Tasks are also generated.'))
+        frappe.get_doc(
+            {"doctype": "Task", "subject": task, "project": project.name}
+        ).insert()
+    frappe.msgprint(_("Tasks are also generated."))
 
 
 def _validate_installation_note(project):
-    in_validation = frappe.db.get_single_value('Project Control Settings', 'in_validation')
-    if in_validation and project.status == 'Completed':
-        installation_notes = frappe.get_all('Installation Note', {'project': project.name})
+    in_validation = frappe.db.get_single_value(
+        "Project Control Settings", "in_validation"
+    )
+    if in_validation and project.status == "Completed":
+        installation_notes = frappe.get_all(
+            "Installation Note", {"project": project.name}
+        )
         if not installation_notes:
-            frappe.throw(_('Please make an Installation Note first before closing the project'.format(project.name)))
+            frappe.throw(
+                _(
+                    "Please make an Installation Note first before closing the project".format(
+                        project.name
+                    )
+                )
+            )
 
 
 def _set_budget_total(project):
-    budget_total = reduce(lambda total, budget: total + budget.amount, project.pc_budgets, 0.0)
+    budget_total = reduce(
+        lambda total, budget: total + budget.amount, project.pc_budgets, 0.0
+    )
     project.pc_budget_total = budget_total
 
 
 def _set_variation_total(project):
-    variation_total = reduce(lambda total, budget: total + budget.amount, project.pc_variations, 0.0)
+    variation_total = reduce(
+        lambda total, budget: total + budget.amount, project.pc_variations, 0.0
+    )
     project.pc_variation_total = variation_total
 
 
 def _set_estimated_total(project):
-    project.pc_estimated_total = sum([project.pc_budget_total, project.pc_variation_total])
+    project.pc_estimated_total = sum(
+        [project.pc_budget_total, project.pc_variation_total]
+    )
     if not project.estimated_costing:
         project.estimated_costing = project.pc_estimated_total
-    estimated_gross_margin, estimated_gross_margin_per = calculate_estimated_gross_margin(project)
+    (
+        estimated_gross_margin,
+        estimated_gross_margin_per,
+    ) = calculate_estimated_gross_margin(project)
     project.pc_estimated_gross_margin = estimated_gross_margin
     project.pc_estimated_gross_margin_per = estimated_gross_margin_per
